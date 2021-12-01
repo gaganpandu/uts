3d4b2182987a817bc739b1080fb62ca99333a763
gagan <95356767+gaganpandu@users.noreply.github.com>
2021-12-01 20:49:34 +0700

    Update produk.dart

--- lib/pages/produk.dart
+++ lib/pages/produk.dart
@@ -20,31 +20,31 @@ class ProductModel {
           namaproduk: 'Instax Mini Mint 7+',
           gambar: 'assets/images/instax_mini_7_plus_mint_r.png',
           harga: 49.90,
-          kodewarna: 0xff70b1a1,
+          kodewarna: 0xFF212121,
         ),
         ProductModel(
           namaproduk: 'Instax Mini Blue 7+',
           gambar: 'assets/images/instax_mini_7_plus_blue_r.png',
           harga: 50.90,
-          kodewarna: 0xff77a0c6,
+          kodewarna: 0xFF212121,
         ),
         ProductModel(
           namaproduk: 'Instax Mini Choral 7+',
           gambar: 'assets/images/instax_mini_7_plus_choral_r.png',
           harga: 51.90,
-          kodewarna: 0xffb0463c,
+          kodewarna: 0xFF212121,
         ),
         ProductModel(
           namaproduk: 'Instax Mini Pink 7+',
           gambar: 'assets/images/instax_mini_7_plus_pink_r.png',
           harga: 52.90,
-          kodewarna: 0xfffcf9496,
+          kodewarna: 0xFF212121,
         ),
         ProductModel(
           namaproduk: 'Instax Mini Lavender 7+',
           gambar: 'assets/images/instax_mini_7_plus_lavender_r.png',
           harga: 53.90,
-          kodewarna: 0xff855f8c,
+          kodewarna: 0xFF212121,
         ),
       ];
 }
